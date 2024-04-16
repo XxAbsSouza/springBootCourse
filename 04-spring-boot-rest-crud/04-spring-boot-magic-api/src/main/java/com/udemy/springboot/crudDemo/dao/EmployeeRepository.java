01ab8141767303aef892bab810afcfbdcb352091
Ana Beatriz  dos Santos Souza <beatriz_ana58@hotmail.com>
2024-04-16 12:05:28 -0300

    .

--- 04-spring-boot-rest-crud/04-spring-boot-magic-api/src/main/java/com/udemy/springboot/crudDemo/dao/EmployeeRepository.java
+++ 04-spring-boot-rest-crud/04-spring-boot-magic-api/src/main/java/com/udemy/springboot/crudDemo/dao/EmployeeRepository.java
@@ -2,7 +2,9 @@ package com.udemy.springboot.crudDemo.dao;
 
 import com.udemy.springboot.crudDemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path = "members") //--> muda o endpoint para o escrito entre ""
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
     //JpaRepository<Employee - nossa classe, Integer - id da classe>
 }
