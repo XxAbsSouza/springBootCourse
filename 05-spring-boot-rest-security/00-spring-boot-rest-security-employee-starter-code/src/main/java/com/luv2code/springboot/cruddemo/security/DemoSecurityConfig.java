a3a1dac2006290c8b5021ab4f98cdb9716e30e85
Ana Beatriz  dos Santos Souza <beatriz_ana58@hotmail.com>
2024-04-23 17:13:34 -0300

    tudofuncionando

--- 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
+++ 05-spring-boot-rest-security/00-spring-boot-rest-security-employee-starter-code/src/main/java/com/luv2code/springboot/cruddemo/security/DemoSecurityConfig.java
@@ -1,16 +1,82 @@
-//Quando criamos esse arquivo, o spring security nao vai usar o application.propertiers
-//ao inves disso, ele usara esses usuarios definidos aqui
+package com.luv2code.springboot.cruddemo.security;////Quando criamos esse arquivo, o spring security nao vai usar o application.propertiers
+////ao inves disso, ele usara esses usuarios definidos aqui
+//
+//package com.luv2code.springboot.cruddemo.security;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.security.provisioning.JdbcUserDetailsManager;
+//import org.springframework.security.provisioning.UserDetailsManager;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import javax.sql.DataSource;
+//
+//@Configuration
+//public class DemoSecurityConfig {
+//    //add support for jdbc
+//    @Bean
+//    public UserDetailsManager userDetailsManager(DataSource dataSource){
+//        return  new JdbcUserDetailsManager(dataSource);
+//    }
+//    @Bean
+//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+//        //Isso vai setar os roles que tem autorizacao para cada endpoint
+//        http.authorizeHttpRequests(configurer ->
+//                configurer
+//                        .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+//                        .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+//                        .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+//                        .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+//                        .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+//
+//        );
+//
+//        //use http basic authentication
+//        http.httpBasic(Customizer.withDefaults());
+//
+//        //disable Cross Site Request Forgery(CSRF)
+//        //in general, not required for stateless rest aPIS that use POST,PUT,DELETE and/or PATCH
+//        http.csrf(csrf -> csrf.disable());
+//
+//        return http.build();
+//    }
+//
+////    @Bean
+////    public InMemoryUserDetailsManager userDetailsManager(){
+////        UserDetails john = User.builder()
+////                .username("john")
+////                .password("{noop}test123")
+////                .roles("EMPLOYEE")
+////                .build();
+////        UserDetails mary = User.builder()
+////                .username("mary")
+////                .password("{noop}test123")
+////                .roles("EMPLOYEE", "MANAGER")
+////                .build();
+////        UserDetails susan = User.builder()
+////                .username("susan")
+////                .password("{noop}test123")
+////                .roles("EMPLOYEE", "MANAGER", "ADMIN")
+////                .build();
+////        return new InMemoryUserDetailsManager(john,mary,susan);
+////    }
+//}
+//
 
-package com.luv2code.springboot.cruddemo.security;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
@@ -19,14 +85,20 @@ import javax.sql.DataSource;
 
 @Configuration
 public class DemoSecurityConfig {
-    //add support for jdbc
+
+
     @Bean
-    public UserDetailsManager userDetailsManager(DataSource dataSource){
-        return  new JdbcUserDetailsManager(dataSource);
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        //Isso vai setar os roles que tem autorizacao para cada endpoint
         http.authorizeHttpRequests(configurer ->
                 configurer
                         .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
@@ -34,36 +106,11 @@ public class DemoSecurityConfig {
                         .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
                         .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
                         .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
-
         );
 
-        //use http basic authentication
         http.httpBasic(Customizer.withDefaults());
-
-        //disable Cross Site Request Forgery(CSRF)
-        //in general, not required for stateless rest aPIS that use POST,PUT,DELETE and/or PATCH
         http.csrf(csrf -> csrf.disable());
 
         return http.build();
     }
-
-//    @Bean
-//    public InMemoryUserDetailsManager userDetailsManager(){
-//        UserDetails john = User.builder()
-//                .username("john")
-//                .password("{noop}test123")
-//                .roles("EMPLOYEE")
-//                .build();
-//        UserDetails mary = User.builder()
-//                .username("mary")
-//                .password("{noop}test123")
-//                .roles("EMPLOYEE", "MANAGER")
-//                .build();
-//        UserDetails susan = User.builder()
-//                .username("susan")
-//                .password("{noop}test123")
-//                .roles("EMPLOYEE", "MANAGER", "ADMIN")
-//                .build();
-//        return new InMemoryUserDetailsManager(john,mary,susan);
-//    }
 }
